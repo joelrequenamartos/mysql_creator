b67c767ba73b0a67b99f0ea7763283bc60b8097b
root <root@jrequena.zitro-local>
2022-04-27 16:50:11 +0200

    commit3

--- diccionario.py
+++ diccionario.py
@@ -1,5 +1,8 @@
 import MySQLdb
 import mysql.connector
+import os
+
+salir = False
 server = MySQLdb.connect(
   host = "localhost",
   user = "root",
@@ -35,11 +38,36 @@ def showdatabase():
     for (databases) in cur:
       print("·", databases[0])
 
+def selectDatabase():
+    inputdatabase = input()
+    dataBase = mysql.connector.connect(
+        host ="localhost",
+        user ="root",
+        passwd ="direla"
+        database = inputdatabase
+    )
+
+    cursor = dataBase.cursor()
+
+def insertdatabase():
+    print("Quieres insertar datos en alguna BBD?: ")
+    print("(S)i, (N)o")
+    
+    if insertardatos == 'Si' or insertardatos == 'S':
+        print("Que base de datos quieres usar?: ")
+        showdatabase()
+    insertardatos = input()
+
+    if insertardatos == 'No' or insertardatos == 'N':
+        os.system("exit")
+
 def crearbdd():
     print("Inserta el nombre de la nueva base de datos")
     nombreBDD = input()
     cursorObject = dataBase.cursor()
     cursorObject.execute('CREATE DATABASE %s' %nombreBDD)
+
+    selectDatabase()
 
 def borrarbdd():
     print('Escribe la base de datos que quieres borrar: ')
